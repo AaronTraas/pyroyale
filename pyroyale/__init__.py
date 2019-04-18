0476a7acd76ff772b943c4341c3f977880447089
Aaron Traas <aaron@traas.org>
2019-04-18 10:26:50 -0400

    Cleaned imports

--- pyroyale/__init__.py
+++ pyroyale/__init__.py
@@ -1,3 +1,3 @@
 from .api import ClashRoyaleAPI
-from .errors import ClashRoyaleAPIError, ClashRoyaleAPIMissingFieldsError, ClashRoyaleAPIAuthenticationError, ClashRoyaleAPIClanNotFound
+from .errors import *
 from ._version import __version__
